b467bd87dc00692f1c136e461783f40a98573ad3
Son <soja0529@gmail.com>
2024-01-31 04:07:39 +0900

    풀 하기전 커밋

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -4,9 +4,12 @@ import uploadImage from './content/upload-icon.png';
 
 function Content() {
     const fileInputRef = useRef(null);
+    // useRef를 사용해서 파일 입력 요소를 참조
 
     const uploadFiles = async () => {
         const fileInput = fileInputRef.current;
+        // 이벤트 리스너를 추가/제거해서 중복 이벤트 리스너 등록을 방지
+
         if (fileInput) {
             fileInput.setAttribute('multiple', true);
             fileInput.click();
@@ -19,18 +22,22 @@ function Content() {
             const selectedFiles = fileInput.files;
             if (selectedFiles.length > 0) {
                 const formData = new FormData();
-
+    
                 // 선택한 모든 파일을 FormData에 추가
                 for (let i = 0; i < selectedFiles.length; i++) {
                     formData.append('files', selectedFiles[i]);
                 }
-
+    
                 try {
                     const response = await fetch('http://localhost:8000/group/album/upload', {
                         method: 'POST',
+                        headers: {
+                            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+                            // 추가된 부분: 인증 토큰을 헤더에 포함시킵니다.
+                        },
                         body: formData
                     });
-
+    
                     if (response.ok) {
                         alert('파일 업로드 성공!');
                     } else {
