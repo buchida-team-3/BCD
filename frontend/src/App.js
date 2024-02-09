8c676d35c42b0771416ebc47ef36f451d2ac5d2e
Son <soja0529@gmail.com>
2024-02-10 00:42:51 +0900

    타임랩스 구현 전 커밋

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './components/MainPage';
-import LoginSignup from './components/LoginSignup';
-import ImagePage from './components/ImagePage';
-import UploadPage from './components/UploadPage';
-import UploadPage2 from './components/UploaePage2';
-import Edit from './components/Edit';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import LoginSignup from "./components/LoginSignup";
+import ImagePage from "./components/ImagePage";
+import UploadPage from "./components/UploadPage";
+import UploadPage2 from "./components/UploaePage2";
+import Edit from "./components/Edit";
 
 function App() {
   return (
-    
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/loginandsignup" element={<LoginSignup />} />
-          <Route path="/imagepage" element={<ImagePage />} />
-          <Route path="/uploadpage" element={<UploadPage />} />
-          <Route path="/uploadpage2" element={<UploadPage2 />} />
-          <Route path="/edit" element={<Edit />} />
-        </Routes>
-      </Router>
-   
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/loginandsignup" element={<LoginSignup />} />
+        <Route path="/imagepage" element={<ImagePage />} />
+        <Route path="/uploadpage" element={<UploadPage />} />
+        <Route path="/uploadpage2" element={<UploadPage2 />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </Router>
   );
 }
 
