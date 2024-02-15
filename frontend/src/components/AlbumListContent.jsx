a130f9b3e36b95c2265860f1c5ab4978958d565f
Son <soja0529@gmail.com>
2024-02-15 14:18:23 +0900

    발표전 커밋2

--- frontend/src/components/AlbumListContent.jsx
+++ frontend/src/components/AlbumListContent.jsx
@@ -3,17 +3,21 @@ import { Canvas } from '@react-three/fiber';
 import { Html } from '@react-three/drei';
 import { ScrollControls, Scroll } from './AlbumListScrollControls';
 import bgImage from './content/background.jpg';
-import pageImage from './images/image.jpg';
 import './AlbumListContent.css';
+import image1 from './images/image1.jpeg';
+import image2 from './images/image2.jpeg';
+import image3 from './images/image3.jpeg';
+import image4 from './images/image4.jpeg';
+import image5 from './images/image5.jpeg';
 
-const PageCover = ({ children, title }) => (
+const images = [image1, image2, image3, image4, image5];
+
+const PageCover = ({ children, title, image }) => (
   <div className="page-cover2">
     <div className="image-container">
-      {/* 이미지 컨테이너 추가 */}
-      <img src={pageImage} alt="Page" className="cover-image"/>
+      <img src={image} alt="Page" className="cover-image"/>
     </div>
     <div className="title-container">
-      {/* 텍스트 컨테이너 추가 */}
       <Text title={title} />
     </div>
     {children}
@@ -24,16 +28,13 @@ const Text = ({ title }) => (
   <h2 style={{ color: '#ffffff' }}>{title}</h2>
 );
 
-function Page({ title, position }) {
+function Page({ title, position, image }) {
   return (
     <group position={position}>
       <Html>
         <group>
-          <PageCover>
-            {/* <img src={pageImage} alt="Page" style={{ width: '90%', height: '90%' }} /> */}
-            <Text title={title} />
+          <PageCover image={image} title={title}>
           </PageCover>
-          {/* <Text title={title} /> */}
         </group>
       </Html>
     </group>
@@ -41,20 +42,20 @@ function Page({ title, position }) {
 }
 
 export default function AlbumListContent() {
-  const pages = Array.from({ length: 10 }).map((_, index) => {
-    // 가로 1열로 배치하기 위해 y 위치를 고정하고, x 위치를 조정하여 각 페이지가 가로로 표시되도록 합니다.
-    const x = index * 6.0 - 5.2; // 각 페이지 간의 가로 간격을 조정합니다.
+  const pages = Array.from({ length: 5 }).map((_, index) => {
+    const image = images[index];
+    const x = index * 6.0 - 5.2;
     const y = 3.1;
-    return { title: `Page ${index + 1}`, position: [x, y, 0] };
+    return {  position: [x, y, 0], image };
   });
 
   return (
     <Canvas gl={{ antialias: false }} dpr={[1, 1.5]} style={{ backgroundImage: `url(${bgImage})`, width: '100%', height: '100vh' }}>
       <Suspense fallback={null}>
-        <ScrollControls pages={2.6} distance={1} damping={4} horizontal={true} infinite={false}> {/* horizontal 속성을 true로 변경하여 가로 스크롤링을 활성화합니다. */}
+        <ScrollControls pages={2.6} distance={1} damping={4} horizontal={true} infinite={false}>
           <Scroll>
             {pages.map((page, index) => (
-              <Page key={index} title={page.title} position={page.position} />
+              <Page key={index} title={page.title} position={page.position} image={page.image} />
             ))}
           </Scroll>
         </ScrollControls>
