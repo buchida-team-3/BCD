d2712086ccaac1e570d8442c10a645b137fb507b
Son <soja0529@gmail.com>
2024-02-15 10:10:45 +0900

    네비바, 로고 등 자잘한 수정

--- frontend/src/components/ImageOverlay.jsx
+++ frontend/src/components/ImageOverlay.jsx
@@ -8,7 +8,7 @@ function ImageOverlay() {
           style={{ position: 'absolute', bottom: 43, left: 80, fontSize: '20px', color: "white"}}>
           뒤로 가기
         </a>
-        <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '20px', color: "white" }}>Mountain</div>
+        <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '20px', color: "white" }}>Gallery</div>
         {/* <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>10/15/2021</div> */}
       </div>
     )
