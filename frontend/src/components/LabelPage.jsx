e308ae30f51729ae3343b0fd39d94495bbc59002
Jiho <jiho001108@gmail.com>
2024-02-16 22:36:29 +0900

    정렬 구현 중(1)

--- frontend/src/components/LabelPage.jsx
+++ frontend/src/components/LabelPage.jsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Preload, Image as ImageImpl } from '@react-three/drei'
 import { ScrollControls, Scroll, useScroll } from './LabelScrollControls'
@@ -11,11 +11,17 @@ import LabelContent from './LabelContent'
 import './LabelPage.css'
 
 function LabelPage() {
+    const [filterLabel, setFilterLabel] = useState("Filtering");
+
+    const toggleFilterLabel = () => {
+        setFilterLabel(currentLabel => currentLabel === "Filtering" ? "All" : "Filtering");
+    };
+
     return (
         <>
         <Navbar />
-        <LabelContent />
-        <LabelOverlay />
+        <LabelContent filterLabel={filterLabel} />
+        <LabelOverlay onToggleFilterLabel={toggleFilterLabel} filterLabel={filterLabel} />
         </>
     )
 }
