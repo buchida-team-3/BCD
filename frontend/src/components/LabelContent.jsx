f815899b881d53cd7680e010a69c8c824fdcc519
Jiho <jiho001108@gmail.com>
2024-02-18 13:31:29 +0900

    정렬 페이지-편집 페이지 연결 중

--- frontend/src/components/LabelContent.jsx
+++ frontend/src/components/LabelContent.jsx
@@ -1,25 +1,31 @@
-import * as THREE from 'three';
-import axios from 'axios';
+import * as THREE from "three";
+import axios from "axios";
 
-import { Suspense, useRef, useEffect, useState } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Preload, Image as ImageImpl } from '@react-three/drei';
-import { ScrollControls, Scroll, useScroll } from './LabelScrollControls';
-import { useImageData } from './ImageContext';
+import { React, Suspense, useRef, useEffect, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Preload, Image as ImageImpl, OrbitControls } from "@react-three/drei";
+import { ScrollControls, Scroll, useScroll } from "./LabelScrollControls";
+import { useImageData } from "./ImageContext";
 
-import bgImage from './content/background.jpg';
+import bgImage from "./content/background.jpg";
 
 function Image(props) {
   const ref = useRef();
   const group = useRef();
-  // useFrame에서 흑백 처리 관련 코드를 제거합니다.
+
   useFrame((state, delta) => {
-    group.current.position.z = THREE.MathUtils.damp(group.current.position.z, Math.max(0, 0), 4, delta);
+    group.current.position.z = THREE.MathUtils.damp(
+      group.current.position.z,
+      Math.max(0, 0),
+      4,
+      delta
+    );
   });
 
   const handleClick = () => {
-    const imageName = props.url.split('/').pop().split('.').shift();
-    window.location.href = `/edit/${imageName}`;
+    const imageName = props.url.split("/").pop().split(".").shift();
+    // window.location.href = `/edit/${imageName}`; // edit 페이지로 가되, 헤더에 이미지 이름을 넘겨줌
+    window.location.href = `/edit?selectedImageForEdit=${imageName}`;
   };
 
   return (
@@ -42,7 +48,14 @@ function Page({ m = 0.4, urls, ...props }) {
   return (
     <group {...props}>
       {urls.map((url, index) => (
-        <Image key={index} position={positions[index]} scale={scale} url={url} />
+        <Image
+          key={index}
+          position={positions[index]}
+          // rotation={[0, 45*Math.PI/180, 0]} // 모든 이미지 y축에 대해 45도 회전
+          // opacity={0.5} transparent={true} // 투명도 조절 
+          scale={scale}
+          url={url}
+        />
       ))}
     </group>
   );
@@ -57,94 +70,229 @@ function Pages({ imageGroups }) {
     <>
       {imageGroups.map((group, index) => {
         // Determine the vertical position: all items in the first row, then all items in the second row
-        const y = index < imageGroups.length / 2 ? -viewport.height / 4 : viewport.height / 4;
+        const y =
+          index < imageGroups.length / 2
+            ? -viewport.height / 4
+            : viewport.height / 4;
         // Determine the horizontal position: incrementally increase based on the group index
         // For the second row, reset the index to start from 0 again
-        const xOffset = index < imageGroups.length / 2 ? index : index - (imageGroups.length / 2);
-        const x = -(viewport.width / 2) + (xOffset * groupWidth) + groupWidth / 2; // Adjust this calculation as needed
+        const xOffset =
+          index < imageGroups.length / 2
+            ? index
+            : index - imageGroups.length / 2;
+        const x = -(viewport.width / 2) + xOffset * groupWidth + groupWidth / 2; // Adjust this calculation as needed
 
-        return (
-          <Page
-            key={index}
-            urls={group}
-            position={[x, y, 0]}
-          />
-        );
+        return <Page key={index} urls={group} position={[x, y, 0]} />;
       })}
     </>
   );
 }
 
 export default function LabelContent({ filterLabel }) {
-  const [ imageGroups, setImageGroups ] = useState([]);
-
   // imageData: 이미지 데이터 자체, setImageData: 이미지 데이터를 변경하는 함수
   const { imageData, setImageData } = useImageData(); // Context API 사용을 위해 추가 -> 여기에서 이미 imageData를 사용할 준비가 됨
-  
-  console.log('filterLabel 변경:', filterLabel);
+  const [ imageGroups, setImageGroups ] = useState([]);
+  const [ fetchAttempted, setFetchAttempted ] = useState(false);
+
+  // filterLabel = all -> 정렬된 이미지 보임, filterLabel = Filtering -> 전체 이미지 보임
+  console.log("filterLabel 변경:", filterLabel);
   useEffect(() => {
-    const fetchImages = async () => {
-      // filterLabel 상태에 따라 로직 변경
-      const endpoint = filterLabel === "Filtering" ? '/api/all' : '/api/filter';
-      try {
-        const response = await axios.get(`http://localhost:8000${endpoint}`, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
-            'Label': filterLabel
-          }
-        });
-        
-        // 이미지 데이터를 로컬 스토리지에 저장 -> 현재 response.data는 image의 모든 정보를 담고 있음
-        // 예시:
-        // {
-        // class_name: "['person']",
-        // id: 114,
-        // image_edited: false,
-        // image_lable_rgb: 2,
-        // image_meta: "('2023:04:09 10:41:58', 37.513930555555554, 127.10469166666667)",
-        // image_name: "IMG_2206.jpeg",
-        // image_path: "https://jungle-buchida-s3.s3.ap-northeast-2.amazonaws.com/img_20/IMG_2206.jpeg",
-        // user_id: 8
-        // }
+    // 이미지 데이터가 비어있고 요청이 시도되지 않았거나, 데이터가 있는 경우에만 요청
+    if (!fetchAttempted || (fetchAttempted && imageData.length > 0)) {
+      const fetchImages = async () => {
+        // filterLabel 상태에 따라 로직 변경
+        const endpoint =
+          filterLabel === "Filtering" ? "/api/all" : "/api/filter";
+        try {
+          const response = await axios.get(`http://localhost:8000${endpoint}`, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+              Label: filterLabel,
+            },
+          });
 
-        // Context API: 이미지 데이터를 전역 상태로 관리
-        setImageData(response.data);
+          // response.data(=imageData): image의 모든 정보를 담고 있음
+          // 예시:
+          // {
+          // class_name: "['person']",
+          // id: 114,
+          // image_edited: false,
+          // image_lable_rgb: 2,
+          // image_meta: "('2023:04:09 10:41:58', 37.513930555555554, 127.10469166666667)",
+          // image_name: "IMG_2206.jpeg",
+          // image_path: "https://jungle-buchida-s3.s3.ap-northeast-2.amazonaws.com/img_20/IMG_2206.jpeg",
+          // user_id: 8
+          // }
 
-        // 이미지 3장씩 그룹화, 전역으로 관리되는 이미지 데이터를 사용
-        const image_path = response.data.map((image) => image.image_path);
-        const formattedGroups = image_path.map((_, index, array) => 
-          index % 3 === 0 ? array.slice(index, index + 3) : null
-        ).filter(Boolean);
+          // Context API: 이미지 데이터를 전역 상태로 관리
+          setImageData(response.data);
+
+          // 이미지 3장씩 그룹화, 전역으로 관리되는 이미지 데이터를 사용
+          const image_path = response.data.map((image) => image.image_path);
+          const formattedGroups = image_path
+            .map((_, index, array) =>
+              index % 3 === 0 ? array.slice(index, index + 3) : null
+            )
+            .filter(Boolean);
+
+          setImageGroups(formattedGroups);
+        } catch (error) {
+          console.error("Failed to fetch images:", error);
+          window.location.href = "/loginandsignup";
+        }
+      };
+      console.log("LabelContent.jsx: 이미지 데이터 로드 중...");
+
+      // 이미 로드된 이미지 데이터 가져오기
+      // 이미지의 캐시 여부 확인
+      if (imageData.length === 0) {
+        console.log("LabelContent.jsx: 이미지 데이터가 메모리에 없음");
+        fetchImages();
+        setFetchAttempted(true); // 무한 루프 방지
+      } else {
+        console.log("LabelContent.jsx: 이미지 데이터가 메모리에 있음");
+        const image_path = imageData.map((image) => image.image_path);
+        const formattedGroups = image_path
+          .map((_, index, array) =>
+            index % 3 === 0 ? array.slice(index, index + 3) : null
+          )
+          .filter(Boolean);
 
         setImageGroups(formattedGroups);
-
-      } catch (error) {
-        console.error('Failed to fetch images:', error);
-        window.location.href = '/loginandsignup';
       }
-    };
 
-    // 이미 로드된 이미지 데이터 가져오기
-    // 이미지의 캐시 여부 확인
-    if (imageData.length === 0) {
-      console.log('LabelContent.jsx: 이미지 데이터 캐시에 없음');
-      fetchImages();
-    } else {
-      console.log('LabelContent.jsx: 이미지 데이터 캐시에 있음');
-      const image_path = imageData.map((image) => image.image_path);
-      const formattedGroups = image_path.map((_, index, array) => 
-        index % 3 === 0 ? array.slice(index, index + 3) : null
-      ).filter(Boolean);
+      if (filterLabel === "All") {
+        // const class_name = imageData.map((image) => image.class_name);
+        // const id = imageData.map((image) => image.id);
+        // const image_edited = imageData.map((image) => image.image_edited);
+        // const image_meta = imageData.map((image) => image.image_meta);
+        // const image_name = imageData.map((image) => image.image_name);
+        // const user_id = imageData.map((image) => image.user_id);
 
-      setImageGroups(formattedGroups);
+        // theme 종류(11개): car, inside, animal, vehicle, person,
+        //            electronic, dish, food, sport, accessory, landscape
+
+        // rgb 값에 따라 이미지 그룹화
+        const rgb_0_paths = imageData
+          .filter((image) => image.image_lable_rgb === 0)
+          .map((image) => image.image_path);
+        // console.log("rgb_0_paths:", rgb_0_paths);
+
+        const rgb_1_paths = imageData
+          .filter((image) => image.image_lable_rgb === 1)
+          .map((image) => image.image_path);
+        // console.log("rgb_1_paths:", rgb_1_paths);
+
+        const rgb_2_paths = imageData
+          .filter((image) => image.image_lable_rgb === 2)
+          .map((image) => image.image_path);
+        // console.log("rgb_2_paths:", rgb_2_paths);
+
+        // 테마별로 이미지 그룹화 && 공간 별로 이미지 그룹화 추가해보자
+        const themeCarPaths = imageData.filter(
+          (image) => image.theme === "car"
+        );
+        const themeInsidePaths = imageData.filter(
+          (image) => image.theme === "inside"
+        );
+        const themeAnimalPaths = imageData.filter(
+          (image) => image.theme === "animal"
+        );
+        const themeVehiclePaths = imageData.filter(
+          (image) => image.theme === "vehicle"
+        );
+        const themePersonPaths = imageData.filter(
+          (image) => image.theme === "person"
+        );
+        const themeElectronicPaths = imageData.filter(
+          (image) => image.theme === "electronic"
+        );
+        const themeDishPaths = imageData.filter(
+          (image) => image.theme === "dish"
+        );
+        const themeFoodPaths = imageData.filter(
+          (image) => image.theme === "food"
+        );
+        const themeSportPaths = imageData.filter(
+          (image) => image.theme === "sport"
+        );
+        const themeAccessoryPaths = imageData.filter(
+          (image) => image.theme === "accessory"
+        );
+        const themeLandscapePaths = imageData.filter(
+          (image) => image.theme === "landscape"
+        );
+
+        // 로깅 -> 경로가 떠야하는데 안뜸
+        console.log("themeCarPaths:", themeCarPaths);
+        console.log("themeInsidePaths:", themeInsidePaths);
+        console.log("themeAnimalPaths:", themeAnimalPaths);
+        console.log("themeVehiclePaths:", themeVehiclePaths);
+        console.log("themePersonPaths:", themePersonPaths);
+        console.log("themeElectronicPaths:", themeElectronicPaths);
+        console.log("themeDishPaths:", themeDishPaths);
+        console.log("themeFoodPaths:", themeFoodPaths);
+        console.log("themeSportPaths:", themeSportPaths);
+        console.log("themeAccessoryPaths:", themeAccessoryPaths);
+        console.log("themeLandscapePaths:", themeLandscapePaths);
+
+        // 테마별로 대표 이미지 3장 선택하는 함수
+        const selectTopThree = (images) => images.slice(0, 3);
+
+        // 각 테마별로 대표 이미지 3장 선택
+        const topRgb0 = selectTopThree(rgb_0_paths);
+        const topRgb1 = selectTopThree(rgb_1_paths);
+        const topRgb2 = selectTopThree(rgb_2_paths);
+        const topCar = selectTopThree(themeCarPaths);
+        const topInside = selectTopThree(themeInsidePaths);
+        const topAnimal = selectTopThree(themeAnimalPaths);
+        const topVehicle = selectTopThree(themeVehiclePaths);
+        const topPerson = selectTopThree(themePersonPaths);
+        const topElectronic = selectTopThree(themeElectronicPaths);
+        const topDish = selectTopThree(themeDishPaths);
+        const topFood = selectTopThree(themeFoodPaths);
+        const topSport = selectTopThree(themeSportPaths);
+        const topAccessory = selectTopThree(themeAccessoryPaths);
+        const topLandscape = selectTopThree(themeLandscapePaths);
+
+        const allImageGroups = [
+          topRgb0,
+          topRgb1,
+          topRgb2,
+          topCar,
+          topInside,
+          topAnimal,
+          topVehicle,
+          topPerson,
+          topElectronic,
+          topDish,
+          topFood,
+          topSport,
+          topAccessory,
+          topLandscape,
+        ]; // 각 테마별로 대표 이미지 3장씩 모인 그룹
+
+        setImageGroups(allImageGroups);
+      }
     }
-
-  }, [ filterLabel, setImageData, imageData ]);
+  }, [filterLabel, setImageData, imageData]);
 
   return (
-    <Canvas gl={{ antialias: false }} dpr={[1, 1.5]} style={{ backgroundImage: `url(${bgImage})` }}>
+    <Canvas
+      gl={{ antialias: false }}
+      dpr={[1, 1.5]}
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <axesHelper scale={10} />
+      <OrbitControls />
       <Suspense fallback={null}>
-        <ScrollControls pages={imageGroups.length / 7} distance={1} damping={4} horizontal={true} infinite={false}>
+        <ScrollControls
+          pages={imageGroups.length / 7}
+          distance={1}
+          damping={4}
+          horizontal={true}
+          infinite={false}
+        >
           <Scroll>
             <Pages imageGroups={imageGroups} />
           </Scroll>
--- frontend/src/components/LabelOverlay.jsx
+++ frontend/src/components/LabelOverlay.jsx
@@ -2,10 +2,8 @@ import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom"; // useNavigate 훅을 임포트합니다.
 
 function LabelOverlay({ onToggleFilterLabel, filterLabel }) {
-  // const [filterLabel, setFilterLabel] = useState("Filtering");
   const fileInputRef = useRef(null);
   const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 초기화합니다.
-
 
   // 파일 업로드 함수
   const uploadFiles = async () => {
@@ -15,7 +13,7 @@ function LabelOverlay({ onToggleFilterLabel, filterLabel }) {
       fileInput.setAttribute("multiple", true);
       fileInput.click();
     }
-    };
+  };
 
   // 파일 선택 변경 이벤트 핸들러
   const handleChange = async (event) => {
@@ -41,9 +39,9 @@ function LabelOverlay({ onToggleFilterLabel, filterLabel }) {
             }
           );
           const responseData = await response.json();
-          
+
           console.log("LabelOverlay.jsx -> responseData:", responseData);
-          
+
           if (response.ok) {
             alert("파일 업로드 성공!");
             window.location.reload();
@@ -59,35 +57,74 @@ function LabelOverlay({ onToggleFilterLabel, filterLabel }) {
 
   // 앨범 생성 페이지로 이동 함수
   const goToCreateAlbumPage = () => {
-    navigate('/createalbum'); // '/createalbum' 경로로 이동합니다.
+    navigate("/createalbum"); // '/createalbum' 경로로 이동합니다.
   };
 
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        pointerEvents: "none",
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <input
         type="file"
         ref={fileInputRef}
         onChange={handleChange}
-        style={{ display: 'none' }}
+        style={{ display: "none" }}
         multiple
       />
       <button
         onClick={goToCreateAlbumPage} // 앨범 생성 버튼 클릭 이벤트
-        style={{ position: 'absolute', bottom: 43, left: 80, fontSize: '20px', color: "white", backgroundColor: "transparent", border: "none", pointerEvents: "auto", cursor: "pointer" }}
+        style={{
+          position: "absolute",
+          bottom: 43,
+          left: 80,
+          fontSize: "20px",
+          color: "white",
+          backgroundColor: "transparent",
+          border: "none",
+          pointerEvents: "auto",
+          cursor: "pointer",
+        }}
       >
         앨범 생성
       </button>
       <button
         onClick={uploadFiles} // 업로드 버튼 클릭 이벤트
-        style={{ position: 'absolute', bottom: 43, right: 80, fontSize: '20px', color: "white", backgroundColor: "transparent", border: "none", pointerEvents: "auto", cursor: "pointer" }}
+        style={{
+          position: "absolute",
+          bottom: 43,
+          right: 80,
+          fontSize: "20px",
+          color: "white",
+          backgroundColor: "transparent",
+          border: "none",
+          pointerEvents: "auto",
+          cursor: "pointer",
+        }}
       >
         업로드
       </button>
       <button
         onClick={onToggleFilterLabel} // 필터 토글 버튼 클릭 이벤트
-        style={{ position: 'absolute', top: 40, left: 40, fontSize: '20px', color: "white", backgroundColor: "transparent", border: "none", pointerEvents: "auto", cursor: "pointer" }}
+        style={{
+          position: "absolute",
+          top: 40,
+          left: 40,
+          fontSize: "20px",
+          color: "white",
+          backgroundColor: "transparent",
+          border: "none",
+          pointerEvents: "auto",
+          cursor: "pointer",
+        }}
       >
-        {filterLabel}
+        {filterLabel === "All" ? "전체보기" : "정렬하기"}
       </button>
     </div>
   );
