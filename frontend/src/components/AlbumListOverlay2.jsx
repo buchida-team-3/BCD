b178eb85edec7da0ce4a5f0787d4db567936ec05
Son <soja0529@gmail.com>
2024-02-19 12:55:18 +0900

    앨범목록페이지2 자잘한 수정

--- frontend/src/components/AlbumListOverlay2.jsx
+++ frontend/src/components/AlbumListOverlay2.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useAlbumList } from './AlbumListContext';
+import CreateAlbumModal from "./CreateAlbumModal";
+import ReactDOM from 'react-dom';
 
 function AlbumListOverlay() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef(null);
+
   const { hoveredCard } = useAlbumList();
   // 뒤로 가기 함수
   const goBack = () => {
       window.history.back();
   };
 
+  // 모달창 열기 수정
+  const openModal = () => {
+    document.body.style.overflow = 'hidden';
+    setIsModalOpen(true);
+  };
+
+  // 모달창 닫기 수정
+  const closeModal = () => {
+    document.body.style.overflow = 'auto';
+    setIsModalOpen(false);
+  };
+
+ // 모달 외부 클릭 감지하여 닫기
+ useEffect(() => {
+  const handleClickOutside = (event) => {
+    if (isModalOpen && modalRef.current && !modalRef.current.contains(event.target)) {
+      closeModal();
+    }
+  };
+
+  document.addEventListener('mousedown', handleClickOutside);
+  return () => document.removeEventListener('mousedown', handleClickOutside);
+}, [isModalOpen]);
+
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
       {hoveredCard && (
         <>
-          <div style={{ position: 'absolute', bottom: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: '20px', color: "white" }}>
+          <div style={{ position: 'absolute', bottom: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: '50px', color: "white" }}>
             여기에 원하는 텍스트
           </div>
           <button onClick={goBack} style={{ position: 'absolute', bottom: '43px', left: '50%', transform: 'translate(-50%, -50%)', fontSize: '20px', color: "white", background: "none", border: "none", cursor: "pointer", pointerEvents: 'all' }}>
@@ -23,6 +52,15 @@ function AlbumListOverlay() {
       <button onClick={goBack} style={{ position: 'absolute', bottom: '43px', left: '80px', fontSize: '20px', color: "white", background: "none", border: "none", cursor: "pointer", pointerEvents: 'all' }}>
         뒤로 가기
       </button>
+      <button onClick={openModal} style={{ position: 'absolute', bottom: '43px', right: '80px', fontSize: '20px', color: "white", background: "none", border: "none", cursor: "pointer", pointerEvents: 'all' }}>
+        앨범 생성
+      </button>
+      {isModalOpen && ReactDOM.createPortal(
+  <div ref={modalRef} onClick={(e) => e.stopPropagation()} style={{zIndex: 10000, position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <CreateAlbumModal onClose={closeModal} />
+  </div>,
+  document.body // 모달을 body 태그의 최상단에 렌더링합니다.
+)}
       <div style={{ position: 'absolute', top: '100px', left: '40px', fontSize: '20px', color: "white" }}>앨범 목록</div>
     </div>
   )
