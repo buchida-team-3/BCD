00be0c9a55420866ca0d3e4b0a735d6e6821d839
Changjo Hwang <hchj2005@gmail.com>
2024-02-21 02:59:49 +0900

    fix: background image 최상위 컨테이너에 적용

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -9,7 +9,6 @@ function MainPage() {
     return (
         <div className="container">
             <NavbarBefore/>
-            <img src={bgImage} alt="background" className="background-image"/>
             <div className={'title-container'}>
                 <div className={'title'}> 붙이다. </div>
                 <div className={'subTitle'}> 우리의 추억을 만들어주는 서비스 </div>
